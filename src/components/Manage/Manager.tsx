e4bb751f0045aef9a11d70e96cea73f88154a20f
TranQuangDuy-51006 <jiren510006@gmail.com>
2025-09-05 16:47:38 +0700

    Manager.tsx

--- src/components/Manage/Manager.tsx
+++ src/components/Manage/Manager.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+// Manager.tsx
+import React, { useEffect, useState } from "react";
 import FormAdd from "./FormAdd";
 import Table from "./Table";
 
+interface DataUser {
+  name: string;
+  money: number;
+  status: "Đã thanh toán" | "Chưa thanh toán";
+}
+
 export default function Manager() {
+  const [list, setList] = useState<DataUser[]>([]);
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("list") || "[]");
+    setList(stored);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
   return (
-    <div className="p-5 bg-gray-100">
+    <div className="p-5 bg-gray-100 min-h-screen">
       <h1 className="text-white bg-orange-400 text-center text-[30px] py-5 rounded-2xl">
         Quản Lý Hóa Đơn Tiền Điện
       </h1>
-      <FormAdd />
-      <Table />
+      <FormAdd list={list} setList={setList} />
+      <Table list={list} setList={setList} />
     </div>
   );
 }
